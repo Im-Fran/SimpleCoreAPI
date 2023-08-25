444cf3d2b0a93bddefa9b8c2f3f4248bfb88725a
Francisco Solis <imfran@duck.com>
2023-08-25 08:36:36 -0400

    Fix NUllPointerException in EntrypointLoader

--- src/main/kotlin/xyz/theprogramsrc/simplecoreapi/standalone/EntrypointLoader.kt
+++ src/main/kotlin/xyz/theprogramsrc/simplecoreapi/standalone/EntrypointLoader.kt
@@ -1,7 +1,6 @@
 package xyz.theprogramsrc.simplecoreapi.standalone
 
 import java.util.Properties
-import java.util.zip.ZipInputStream
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.CONSTRUCTOR)
 @Retention(AnnotationRetention.RUNTIME)
@@ -12,25 +11,27 @@ class EntrypointLoader {
     init {
         // First get the resource 'module.properties' located at the root of the jar file
         val moduleProperties = EntrypointLoader::class.java.getResourceAsStream("/module.properties")
-        // Now read the 'entrypoint' property
-        val entrypoint = (Properties().let {
-            it.load(moduleProperties)
-            it.getProperty("entrypoint")
-        } ?: "").replace("\"", "")
+        if(moduleProperties != null) {
+            // Now read the 'entrypoint' property
+            val entrypoint = (Properties().let {
+                it.load(moduleProperties)
+                it.getProperty("entrypoint")
+            } ?: "").replace("\"", "")
 
-        assert(entrypoint.isNotBlank()) { "Entrypoint cannot be blank!" }
+            assert(entrypoint.isNotBlank()) { "Entrypoint cannot be blank!" }
 
-        // Now load the class
-        val clazz = this::class.java.classLoader.loadClass(entrypoint)
+            // Now load the class
+            val clazz = this::class.java.classLoader.loadClass(entrypoint)
 
-        // Now check if the class itself is an entrypoint, if it is, initialize it, if not check for the first method that is an entrypoint
-        if(clazz.isAnnotationPresent(EntryPoint::class.java)){
-            clazz.getConstructor().newInstance()
-        } else {
-            clazz.methods.forEach { method ->
-                if(method.isAnnotationPresent(EntryPoint::class.java)){
-                    method.invoke(null)
-                    return@forEach
+            // Now check if the class itself is an entrypoint, if it is, initialize it, if not check for the first method that is an entrypoint
+            if(clazz.isAnnotationPresent(EntryPoint::class.java)){
+                clazz.getConstructor().newInstance()
+            } else {
+                clazz.methods.forEach { method ->
+                    if(method.isAnnotationPresent(EntryPoint::class.java)){
+                        method.invoke(null)
+                        return@forEach
+                    }
                 }
             }
         }
