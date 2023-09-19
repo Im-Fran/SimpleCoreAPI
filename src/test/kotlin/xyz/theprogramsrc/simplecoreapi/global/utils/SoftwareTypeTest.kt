1008b041c16a8b47ea0f4546d8371d77470f6e8b
Francisco Solis <imfran@duck.com>
2023-09-19 19:32:32 -0300

    patch(tests): add setup & teardown method

--- src/test/kotlin/xyz/theprogramsrc/simplecoreapi/global/utils/SoftwareTypeTest.kt
+++ src/test/kotlin/xyz/theprogramsrc/simplecoreapi/global/utils/SoftwareTypeTest.kt
@@ -1,16 +1,32 @@
 package xyz.theprogramsrc.simplecoreapi.global.utils
 
+import org.apache.commons.io.FileUtils
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import xyz.theprogramsrc.simplecoreapi.global.SimpleCoreAPI
 import xyz.theprogramsrc.simplecoreapi.standalone.StandaloneLoader
+import java.io.File
 
 internal class SoftwareTypeTest {
 
     @Test
     fun `Test SoftwareType detects Standalone mode`() {
-        StandaloneLoader()
-
         assertEquals(SoftwareType.STANDALONE, SimpleCoreAPI.instance.softwareType)
+    }
+
+    companion object {
+        @BeforeAll
+        @JvmStatic
+        fun setUp() {
+            StandaloneLoader()
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun tearDown() {
+            arrayOf("SimpleCoreAPI/", "plugins/").map { File(it) }.filter{ it.exists() }.forEach { FileUtils.forceDelete(it) }
+        }
     }
 }
