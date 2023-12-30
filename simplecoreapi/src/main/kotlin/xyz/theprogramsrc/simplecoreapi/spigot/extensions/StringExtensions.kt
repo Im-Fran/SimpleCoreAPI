5174f587150f7e5c89d1553a100eb17a66b3d535
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2023-12-29 23:02:51 -0300

    feat: added bukkit color utils and bukkit string extensions

---
+++ simplecoreapi/src/main/kotlin/xyz/theprogramsrc/simplecoreapi/spigot/extensions/StringExtensions.kt
@@ -0,0 +1,39 @@
+package xyz.theprogramsrc.simplecoreapi.spigot.extensions
+
+import org.bukkit.ChatColor
+import org.bukkit.Color
+
+/**
+ * Translates the color codes in the string
+ * @param altColorChar The alternate color code character to use
+ *
+ * @return The translated string
+ */
+fun String.bukkitColor(altColorChar: Char = '&'): String = ChatColor.translateAlternateColorCodes(altColorChar, this)
+
+/**
+ * Translates the color codes in the string and formats it
+ * @param args The arguments to format the string with
+ * @return The translated and formatted string
+ */
+fun String.bukkitColorFormat(vararg args: Any): String = String.format(this.bukkitColor(), *args)
+
+/**
+ * Translates the color codes in the string and formats it
+ * @param args The arguments to format the string with
+ * @param altColorChar The alternate color code character to use
+ * @return The translated and formatted string
+ */
+fun String.bukkitColorFormat(altColorChar: Char = '&', vararg args: Any): String = String.format(this.bukkitColor(altColorChar), *args)
+
+/**
+ * Strips the colors from the string
+ * @return The stripped string
+ */
+fun String.bukkitStripColors(): String? = ChatColor.stripColor(this)
+
+/**
+ * Turns this hex string into a bukkit [Color]
+ * @return The color
+ */
+fun String.bukkitToColor(): Color = Color.fromRGB(this.toInt(16))
