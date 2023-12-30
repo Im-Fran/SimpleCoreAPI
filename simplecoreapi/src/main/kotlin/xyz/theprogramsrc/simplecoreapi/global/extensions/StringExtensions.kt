eebb6ce9a431e399e7bc9f4988a59613e6930c38
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2023-12-29 23:01:12 -0300

    feat: added base64 encryption and decryption to StringExtensions.kt

--- simplecoreapi/src/main/kotlin/xyz/theprogramsrc/simplecoreapi/global/extensions/StringExtensions.kt
+++ simplecoreapi/src/main/kotlin/xyz/theprogramsrc/simplecoreapi/global/extensions/StringExtensions.kt
@@ -4,7 +4,12 @@ import com.google.gson.JsonParser
 import com.google.gson.internal.Streams
 import com.google.gson.stream.JsonWriter
 import java.io.StringWriter
+import java.util.*
 
+/**
+ * Gets the current JsonString as a pretty JsonString
+ * @return the pretty JsonString
+ */
 fun String.prettyJson() = JsonParser.parseString(this).let {
     val stringWriter = StringWriter()
     val jsonWriter = JsonWriter(stringWriter)
@@ -13,3 +18,15 @@ fun String.prettyJson() = JsonParser.parseString(this).let {
     Streams.write(it, jsonWriter)
     stringWriter.toString()
 }
+
+/**
+ * Turns this String into a Base64 String
+ * @return the Base64 String
+ */
+fun String.toBase64(): String = Base64.getEncoder().encodeToString(this.toByteArray())
+
+/**
+ * Turns this Base64 String into a normal String
+ * @return the normal String
+ */
+fun String.fromBase64(): String = String(Base64.getDecoder().decode(this))
